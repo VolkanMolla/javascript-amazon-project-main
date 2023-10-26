908d303f543462742546e8caacd2b589e4742438
volkanmolla <volkanmolla11@gmail.com>
2023-10-26 14:19:27 +0300

    update button activated

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,8 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
-if (!cart) {
-    cart = [{
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2
-    }, {
-        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-        quantity: 3
-    }];
-}
 
 
-function saveToStorage() {
+
+export function saveToStorage() {
     localStorage.setItem('cart', JSON.stringify(cart));
 }
 
@@ -55,4 +47,19 @@ export function calculateCartQuantity() {
         cartQuantity += cartItem.quantity;
     });
     return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+    let matchingItem;
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    });
+
+    if (matchingItem) {
+        matchingItem.quantity = newQuantity;
+    }
+    saveToStorage();
+
 }
