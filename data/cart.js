930828efeeeff266f2e8df51b9e8a9e6fb35d03a
volkanmolla <volkanmolla11@gmail.com>
2023-10-26 15:53:17 +0300

    Save data and generate GTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,15 @@
 export let cart = JSON.parse(localStorage.getItem('cart'));
-
+if (!cart) {
+    cart = [{
+        productId: 'myId1',
+        quantity: 2,
+        deliveryOptionId: '1'
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1,
+        deliveryOptionId: '2'
+    }]
+}
 
 
 export function saveToStorage() {
@@ -21,7 +31,8 @@ export function addtoCart(productId, quantity) {
             //productId: productId,
             //quantity: quantity
             productId,
-            quantity
+            quantity,
+            deliveryOptionId: '1'
         });
     }
     saveToStorage();
